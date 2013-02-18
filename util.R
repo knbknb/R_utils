1443b69ecdb2b4fb960fde15ea730180fd5b8a69
Knut Behrends <knb@tnp-online.de>
2013-02-18 17:43:28 +0100

    Add listOfVec2DF()
    from https://github.com/bobthecat/codebox/blob/master/list2DF.r

--- util.R
+++ util.R
@@ -5,7 +5,15 @@
 
 ########################################
 
+## Transform a list of vector of the same length to a data.frame.
 
+#from : https://github.com/bobthecat/codebox/blob/master/list2DF.r
+listOfVec2DF <- function(list){
+l <- as.vector(unlist(lapply(list, length)))
+if(length(unique(l))>1) stop("list elements not some length. Cannot transform to data.frame")
+df <- as.data.frame(matrix(as.vector(unlist(list)), ncol=l[1]))
+return(df)
+}
 
 
 
